47065f9d051d2e276fab3855f0cf7cb6d0e82575
cheilamelo <cheillaa@hotmail.com>
2023-07-12 11:26:53 -0300

    Aula 44. Componentes Controlados

--- pages/estado/formulario.jsx
+++ pages/estado/formulario.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+
 export default function formulario() {
+    const [valor, setValor] = useState(null)
+
+    function alterarInput() {
+        setValor(valor + '!')
+    }
+
     return (
-        <div>
-            <input type="text" />
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column'
+            }}
+        >
+            <span>{valor}</span>
+            <input
+                type="text"
+                value={valor}
+                onChange={e => setValor(e.target.value)}
+            />
+            <button onClick={alterarInput}>Alterar</button>
         </div>
     )
 }
